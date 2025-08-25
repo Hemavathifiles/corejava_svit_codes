a85c4ca9824dc1c3525413d7765c6fcd9085b35e
hemav <hemav@LAPTOP-BH7SCHNS>
2025-08-25 21:10:23 +0530

    first commit

--- corejava_svit/src/org/tnsif/sf/c2tc/oops/BeforeEncapsulation.java
+++ corejava_svit/src/org/tnsif/sf/c2tc/oops/BeforeEncapsulation.java
@@ -11,6 +11,7 @@ public class BeforeEncapsulation {
 
 	public static void main(String[] args) {
 		Human1 obj=new Human1();
+		System.out.println("Age " +obj.age + " "+ "Name "+obj.name);
 		obj.age=26;
 		obj.name="hema";
 		obj.age=87;
